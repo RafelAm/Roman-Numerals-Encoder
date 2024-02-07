d0da0720c26c5cab5d6e6475a65710eb29e06839
Rafel Amengual Tomás <147522281+RafelAm@users.noreply.github.com>
2024-02-07 09:33:20 +0100

    Update RNE.java
    2 soluciones

--- RNE.java
+++ RNE.java
@@ -70,3 +70,78 @@ public class Conversion {
       return  test;
     }
 }
+
+
+
+import java.util.HashMap;
+
+
+public class Conversion {
+
+    public String solution(int n) {
+        
+      HashMap<Integer, String> RomanNumbers = new HashMap<Integer, String>();
+      
+      String test = "";
+      
+      RomanNumbers.put(1, "I");
+      RomanNumbers.put(4, "IV");
+      RomanNumbers.put(5, "V");
+      RomanNumbers.put(9, "IX");
+      RomanNumbers.put(10, "X");
+      RomanNumbers.put(40, "XL");
+      RomanNumbers.put(50, "L");
+      RomanNumbers.put(90, "XC");
+      RomanNumbers.put(100, "C");
+      RomanNumbers.put(400, "CD");
+      RomanNumbers.put(500, "D");
+      RomanNumbers.put(900, "CM");
+      RomanNumbers.put(1000, "M");
+      
+      while(n - 1000 >= 0){
+        n -= 1000;
+        test += RomanNumbers.get(1000);
+      }
+      while(n - 900 >= 0){
+        n -= 900;
+        test += RomanNumbers.get(900);
+      }
+      while(n - 500 >= 0){
+        n -= 500;
+        test += RomanNumbers.get(500);
+      }
+       while(n - 400 >= 0){
+        n -= 400;
+        test += RomanNumbers.get(400);
+      } while(n - 100 >= 0){
+        n -= 100;
+        test += RomanNumbers.get(100);
+      } while(n - 90 >= 0){
+        n -= 90;
+        test += RomanNumbers.get(90);
+      } while(n - 50 >= 0){
+        n -= 50;
+        test += RomanNumbers.get(50);
+      } while(n - 40 >= 0){
+        n -= 40;
+        test += RomanNumbers.get(40);
+      } while(n - 10 >= 0){
+        n -= 10;
+        test += RomanNumbers.get(10);
+      } while(n - 9 >= 0){
+        n -= 9;
+        test += RomanNumbers.get(9);
+      } while(n - 5 >= 0){
+        n -= 5;
+        test += RomanNumbers.get(5);
+      }while(n - 4 >= 0){
+        n -= 4;
+        test += RomanNumbers.get(4);
+      }while(n - 1 >= 0){
+        n -= 1;
+        test += RomanNumbers.get(1);
+      }
+      
+      return  test;
+    }
+}
